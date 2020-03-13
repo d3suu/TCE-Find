75af9d7ec5cd51aaab7d3f9a1b6ee82ced434f1e
d3suu <d3s.prv@mail.ru>
2020-03-13 21:54:06 +0100

    Make base safe, add bs4

--- tcefind.py
+++ tcefind.py
@@ -13,9 +13,14 @@ def webform():
 @app.route('/search', methods=['GET'])
 def parse_search():
     searchword = request.args.get("searchword")
-    indexfile = open('/tmp/TCEFind-Index.cache', 'r')
-    indexcache = indexfile.read()
-    indexfile.close()
+    if searchword is None or searchword == '':
+        return "<head><title> TCE-Find </title></head><body><h1> No search word specified! </h1></body>"
+    try:
+        indexfile = open('/tmp/TCEFind-Index.cache', 'r')
+        indexcache = indexfile.read()
+        indexfile.close()
+    except:
+        return "<head><title> TCE-Find </title></head><body><h1> Index cache error! </h1></body>"
     names = indexcache.splitlines()
 
     # Match in names
@@ -33,8 +38,12 @@ def parse_search():
 @app.route("/info", methods=['GET'])
 def package_info():
     packagename = request.args.get("package")
+    if packagename is None or packagename == '':
+        return "<head><title> TCE-Find </title></head><body><h1> No package specified! </h1></body>"
+    md5sum = requests.get("http://distro.ibiblio.org/tinycorelinux/11.x/x86/tcz/" + packagename + ".md5.txt")
+    if md5sum.status_code != 200:
+        return "<head><title> TCE-Find - " + packagename + "</title></head><body><h1> Package not found! </h1></body>"
     info = requests.get("http://distro.ibiblio.org/tinycorelinux/11.x/x86/tcz/" + packagename + ".info").content.decode("utf-8")
-    md5sum = requests.get("http://distro.ibiblio.org/tinycorelinux/11.x/x86/tcz/" + packagename + ".md5.txt").content.decode("utf-8")
     dep = requests.get("http://distro.ibiblio.org/tinycorelinux/11.x/x86/tcz/" + packagename + ".dep")
     depret = ""
     if dep.status_code == 200:
@@ -42,13 +51,14 @@ def package_info():
         depret = "<h2> Dependency: </h2><br>"
         for x in dep:
             depret += "<a href=\"/info?package=" + x + "\">" + x + "</a><br>"
-    return render_template("info.html", info=info, md5sum=md5sum, dep=depret, packagename=packagename)
+    return render_template("info.html", info=info, md5sum=md5sum.content.decode("utf-8"), dep=depret, packagename=packagename)
 
 def cacheIndex(do_once):
     if do_once:
         index = requests.get("http://distro.ibiblio.org/tinycorelinux/11.x/x86/tcz/")
         if(index.status_code != 200):
             print("ibiblio error!")
+            return # Basically, use last known cache
         soup = BeautifulSoup(index.content, 'html.parser')
         links = soup.find_all('a')
         links = links[4:] # Remove directory links
